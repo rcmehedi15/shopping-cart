b0dc814de1d5e9f7be255e01f4d88794c4ed9314
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-02-17 21:48:43 +0600

    project finish

--- js/case.js
+++ js/case.js
@@ -1,40 +1,38 @@
-// 2nd  cart 
-// iphone case + 
-
 function updateCaseNumber(isIncrease){
     const caseNumberField = document.getElementById('case-number-field');
     const caseNumberString = caseNumberField.value;
     const previousCaseNumber = parseInt(caseNumberString);
 
     let newCaseNumber;
+
     if(isIncrease === true){
         newCaseNumber = previousCaseNumber + 1;
     }
     else{
         newCaseNumber = previousCaseNumber - 1;
     }
-    
+
     caseNumberField.value = newCaseNumber;
 
     return newCaseNumber;
 }
 
-// value multipication 
-
 function updateCaseTotalPrice(newCaseNumber){
-    const caseTotalPrice = newCaseNumber* 59;
+    const caseTotalPrice = newCaseNumber * 59;
     const caseTotalElement = document.getElementById('case-total');
     caseTotalElement.innerText = caseTotalPrice;
-}
-// value increase
-document.getElementById('btn-case-plus').addEventListener('click',function(){
+} 
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
     const newCaseNumber = updateCaseNumber(true);
-    updateCaseTotalPrice(newCaseNumber);
-})
-// value decrease
 
-document.getElementById('btn-case-minus').addEventListener('click',function(){
+    updateCaseTotalPrice(newCaseNumber);
+    calculateSubTotal();
+});
+
+document.getElementById('btn-case-minus').addEventListener('click', function(){
     const newCaseNumber = updateCaseNumber(false);
+    
     updateCaseTotalPrice(newCaseNumber);
+    calculateSubTotal();
 })
-

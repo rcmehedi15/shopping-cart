dfe8e800bf7c981b2bb9c8f0b259e97c77ba1742
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-02-17 18:50:10 +0600

    1st cart working

--- js/case.js
+++ js/case.js
@@ -1,20 +1,40 @@
-// 2nd cart--->>>> + incrase value
+// 2nd  cart 
+// iphone case + 
 
-document.getElementById('btn-case-plus').addEventListener('click',function(){
+function updateCaseNumber(isIncrease){
     const caseNumberField = document.getElementById('case-number-field');
     const caseNumberString = caseNumberField.value;
     const previousCaseNumber = parseInt(caseNumberString);
 
-    const newCaseNumber = previousCaseNumber + 1;
+    let newCaseNumber;
+    if(isIncrease === true){
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else{
+        newCaseNumber = previousCaseNumber - 1;
+    }
+    
     caseNumberField.value = newCaseNumber;
-})
-// 2nd cart--->>>> - decrease value
 
+    return newCaseNumber;
+}
+
+// value multipication 
+
+function updateCaseTotalPrice(newCaseNumber){
+    const caseTotalPrice = newCaseNumber* 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+}
+// value increase
 document.getElementById('btn-case-plus').addEventListener('click',function(){
-    const caseNumberField = document.getElementById('case-number-field');
-    const caseNumberString = caseNumberField.value;
-    const previousCaseNumber = parseInt(caseNumberString);
-
-    const newCaseNumber = previousCaseNumber + 1;
-    caseNumberField.value = newCaseNumber;
+    const newCaseNumber = updateCaseNumber(true);
+    updateCaseTotalPrice(newCaseNumber);
 })
+// value decrease
+
+document.getElementById('btn-case-minus').addEventListener('click',function(){
+    const newCaseNumber = updateCaseNumber(false);
+    updateCaseTotalPrice(newCaseNumber);
+})
+
---
+++ js/phone.js
@@ -0,0 +1,10 @@
+// 1st cart 
+
+document.getElementById('btn-phone-plus').addEventListener('click',function(){
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+
+    const newPhoneNumber = previousPhoneNumber + 1;
+    phoneNumberField.value = newPhoneNumber;
+})
